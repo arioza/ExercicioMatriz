018c1157ed3cfd81d54eebf8ba53247ae439b293
Gabriel de Alencar Arioza <gabriel_alencar95@hotmail.com>
2019-07-04 10:34:37 -0400

    Exercicio de Matriz com C#

---
+++ Exerciciomatriz02/PosicoesMatriz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Exerciciomatriz02
+{
+    public class PosicoesMatriz
+    {
+        
+        public int right(int j)
+        {            
+            return j+1;
+        }
+
+        public int left(int j)
+        {
+            return j-1;
+        }
+
+        public int up(int i)
+        {
+            return i-1;
+        }
+
+        public int down(int i)
+        {
+            return i+1;
+        }
+    }
+}
---
+++ Exerciciomatriz02/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Exerciciomatriz02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PosicoesMatriz matriz = new PosicoesMatriz();
+            int linhas = int.Parse(Console.ReadLine());
+            int colunas = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[linhas, colunas];
+            Console.WriteLine();
+            Console.Write("Celulas da matriz: ");
+            Console.WriteLine(mat.Length);
+            Console.WriteLine();
+            for(int i = 0; i< linhas; i++)
+            {
+                string[] values = Console.ReadLine().Split(" ");
+                for(int j=0; j< colunas; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            Console.Write("Informe um numero para consulta: ");
+            int numeroConsulta = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int j = 0; j < colunas; j++)
+                {
+                    
+                    if (mat[i, j] == numeroConsulta)
+                    {
+                        Console.WriteLine("Position of " + numeroConsulta + ": [" + i + "," + j + "]");
+                        Console.WriteLine();
+
+                        if (j > 0)
+                        {
+                            Console.WriteLine("LEFT: "+mat[i, j - 1]);
+                        }
+
+                        if ((j + 1) < colunas)
+                        {
+                            Console.WriteLine("RIGHT: "+mat[i, j + 1]);
+                        }
+
+                        if (i > 0)
+                        {
+                            Console.WriteLine("UP: "+mat[i - 1, j]);
+                        }
+
+                        if ((i + 1) < linhas)
+                        {
+                            Console.WriteLine("DOWN: "+mat[i + 1, j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
